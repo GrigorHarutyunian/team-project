472b659f66d22d4bbe9df2b0958053efb07c9768
Mnae Vardazaryan <manevardazaryan@gmail.com>
2023-11-14 12:09:53 -0800

    Add autocomplete.js

--- countries/js/filter.js
+++ countries/js/filter.js
@@ -1,16 +1,33 @@
+function clear(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
 function filterSearch() {
-    let input, filter;
-    input = document.getElementById('filter');
-    filter = input.value.toUpperCase();
-    title = document.querySelectorAll('.country .content h3');
+    const input = document.getElementById('filter');
+    const filter = input.value.toUpperCase();
+    const title = document.querySelectorAll('.country .content h3');
+    const autocomplate = document.getElementById('autocomplate');
+
+    clear(autocomplate);
 
     for (i = 0; i < title.length; i++) {
         title_text = title[i].textContent.toUpperCase();
 
         if (title_text.indexOf(filter) > -1 ) {
             title[i].closest('.country').style.display = "";
+            const name = document.createElement('p');
+            name.innerText = title[i].textContent;
+            autocomplate.append(name);
         } else {
-            title[i].closest('.country').style.display = 'none'; 
+            title[i].closest('.country').style.display = 'none';
         }
     }
 }
+
+document.addEventListener('click', () => {
+    const autocomplate = document.getElementById('autocomplate');
+
+    clear(autocomplate);
+})
