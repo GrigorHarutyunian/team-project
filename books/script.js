1c83ee5292d314b80bfd479f4257d2b82fbb727d
Hayk Akunyan <haykak5@gmail.com>
2023-11-15 11:43:50 +0400

    Feature ( Fetch Data From API - Total Results Logic ) Get data from given API and display total results.

--- books/script.js
+++ books/script.js
@@ -1,1 +1,24 @@
-console.log('Books.js');
+const apiUrl = 'https://openlibrary.org/search.json?q=';
+const resultsPerPage = 20;
+
+async function searchBooks() {
+    const bookInput = document.getElementById('bookInput').value;
+    const resultsContainer = document.getElementById('results');
+    const paginationContainer = document.getElementById('pagination');
+
+    const apiUrlFirstPage = `${apiUrl}${bookInput}&limit=${resultsPerPage}`;
+
+    try {
+        const response = await fetch(apiUrlFirstPage);
+        const data = await response.json();
+
+        resultsContainer.innerHTML += `<div class="books__total-results">Total results: ${data.numFound}</div>`;
+    } catch (error) {
+        throw new Error('Error fetching data');
+    }
+}
+document.getElementById('searchForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    searchBooks();
+});
