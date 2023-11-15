e48602461f3cae3681bd6879a5aa2a827f338094
Hayk Akunyan <haykak5@gmail.com>
2023-11-15 12:07:34 +0400

    Feature ( displayResults function ) Create displayResult function which displays first 20 results, added styles for result items.

--- books/script.js
+++ books/script.js
@@ -1,6 +1,21 @@
 const apiUrl = 'https://openlibrary.org/search.json?q=';
 const resultsPerPage = 20;
 
+function displayResults(books) {
+    const resultsContainer = document.getElementById('results');
+
+    books.forEach(book => {
+        resultsContainer.innerHTML += `
+            <div class="books__result-item">
+                <h2><strong>Title:</strong> ${book.title}</h2>
+                <h3><strong>Author Name:</strong> ${book.author_name ? book.author_name.join(', ') : 'N/A'}</h3>
+                <p><strong>First Publish Year:</strong> ${book.first_publish_year || 'N/A'}</p>
+                <p><strong>Subject:</strong> ${book.subject ? book.subject.slice(0, 5).join(', ') : 'N/A'}</p>
+            </div>
+        `;
+    });
+}
+
 async function searchBooks() {
     const bookInput = document.getElementById('bookInput').value;
     const resultsContainer = document.getElementById('results');
@@ -13,6 +28,8 @@ async function searchBooks() {
         const data = await response.json();
 
         resultsContainer.innerHTML += `<div class="books__total-results">Total results: ${data.numFound}</div>`;
+
+        displayResults(data.docs);
     } catch (error) {
         throw new Error('Error fetching data');
     }
