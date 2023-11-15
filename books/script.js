6a7af21d906d8c3ca24b94ae7049d2cd8440e855
Hayk Akunyan <haykak5@gmail.com>
2023-11-15 14:13:04 +0400

    Feature ( AOS Animation ) Add AOS animation for page elements, done JS logic refactoring.

--- books/script.js
+++ books/script.js
@@ -2,6 +2,54 @@ const apiUrl = 'https://openlibrary.org/search.json?q=';
 const resultsPerPage = 20;
 let currentPage = 1;
 
+document.getElementById('searchForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    searchBooks();
+});
+
+document.getElementById('pagination').addEventListener('click', function (event) {
+    if (event.target.tagName === 'BUTTON') {
+        const pageNumber = parseInt(event.target.textContent);
+        if (!isNaN(pageNumber)) {
+            getPage(pageNumber);
+        }
+    }
+});
+
+async function searchBooks() {
+    const bookInput = document.getElementById('bookInput').value;
+    const resultsContainer = document.getElementById('results');
+    const paginationContainer = document.getElementById('pagination');
+
+    resultsContainer.innerHTML = '';
+    paginationContainer.innerHTML = '';
+
+    const apiUrlFirstPage = `${apiUrl}${bookInput}&limit=${resultsPerPage}`;
+
+    try {
+        const response = await fetch(apiUrlFirstPage);
+        const data = await response.json();
+
+        displayResults(data.docs);
+
+        const totalPages = Math.ceil(data.numFound / resultsPerPage);
+        for (let i = 1; i <= totalPages; i++) {
+            const pageButton = document.createElement('button');
+            pageButton.textContent = i;
+            paginationContainer.appendChild(pageButton);
+
+            if (i === currentPage) {
+                pageButton.classList.add('active');
+            }
+        }
+
+        resultsContainer.innerHTML += `<div class="books__total-results" data-aos="flip-up" data-aos-delay="200" data-aos-duration="800" data-aos-easing="ease-in-out" data-aos-mirror="false">Total results: ${data.numFound}</div>`;
+    } catch (error) {
+        throw new Error('Error fetching data');
+    }
+}
+
 async function getPage(pageNumber) {
     currentPage = pageNumber;
 
@@ -38,7 +86,14 @@ function displayResults(books) {
 
     books.forEach(book => {
         resultsContainer.innerHTML += `
-            <div class="books__result-item">
+            <div class="books__result-item"
+                 data-aos="fade-up"
+                 data-aos-delay="200"
+                 data-aos-duration="800"
+                 data-aos-easing="ease-in-out"
+                 data-aos-mirror="false"
+                 data-aos-once="true"
+            >
                 <h2><strong>Title:</strong> ${book.title}</h2>
                 <h3><strong>Author Name:</strong> ${book.author_name ? book.author_name.join(', ') : 'N/A'}</h3>
                 <p><strong>First Publish Year:</strong> ${book.first_publish_year || 'N/A'}</p>
@@ -48,49 +103,5 @@ function displayResults(books) {
     });
 }
 
-async function searchBooks() {
-    const bookInput = document.getElementById('bookInput').value;
-    const resultsContainer = document.getElementById('results');
-    const paginationContainer = document.getElementById('pagination');
-
-    const apiUrlFirstPage = `${apiUrl}${bookInput}&limit=${resultsPerPage}`;
-
-    resultsContainer.innerHTML = '';
-    paginationContainer.innerHTML = '';
-
-    try {
-        const response = await fetch(apiUrlFirstPage);
-        const data = await response.json();
-
-        resultsContainer.innerHTML += `<div class="books__total-results">Total results: ${data.numFound}</div>`;
-
-        displayResults(data.docs);
-
-        const totalPages = Math.ceil(data.numFound / resultsPerPage);
-        for (let i = 1; i <= totalPages; i++) {
-            const pageButton = document.createElement('button');
-            pageButton.textContent = i;
-            paginationContainer.appendChild(pageButton);
-
-            if (i === currentPage) {
-                pageButton.classList.add('active');
-            }
-        }
-    } catch (error) {
-        throw new Error('Error fetching data');
-    }
-}
-document.getElementById('searchForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    searchBooks();
-});
-
-document.getElementById('pagination').addEventListener('click', function (event) {
-    if (event.target.tagName === 'BUTTON') {
-        const pageNumber = parseInt(event.target.textContent);
-        if (!isNaN(pageNumber)) {
-            getPage(pageNumber);
-        }
-    }
-});
+// Initialize AOS
+AOS.init();
