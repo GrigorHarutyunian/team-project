6c06ae1f4d44589ba49dfd4a127847cbd28c1a73
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2023-11-16 20:52:02 +0400

    add searching  possibility,add new style for search input,add back to Home a teg with picture,changing modal video in iframe

--- countries/js/script.js
+++ countries/js/script.js
@@ -1,36 +1,34 @@
 function load() {
-    const load = document.getElementById('loading')
-    load.style.display = 'none';
+  const load = document.getElementById("loading");
+  load.style.display = "none";
 }
 
 setInterval(smoothScroll, 1000);
 
 function smoothScroll() {
+  const topButton = document.getElementById("topButton");
 
-	const topButton = document.getElementById('topButton');
+  topButton.addEventListener("click", clickHandler);
 
-	topButton.addEventListener("click", clickHandler);
+  function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
 
-	function clickHandler(e) {
-		e.preventDefault();
-		const href = this.getAttribute("href");
-		const offsetTop = document.querySelector(href).offsetTop;
-
-		scroll({
-			top: offsetTop,
-			behavior: "smooth"
-		});
-	}
+    scroll({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  }
 }
 
 const date = new Date();
-const year  = date.getFullYear();
-const footer = document.getElementById('footer');
-const copyWrite = document.createElement('p');
+const year = date.getFullYear();
+const footer = document.getElementById("footer");
+const copyWrite = document.createElement("p");
 copyWrite.innerText = `${year} © Copyrights Countries`;
 footer.append(copyWrite);
 
-window.onload = function() {
-    setTimeout(load, 1000);
-}
-  
+window.onload = function () {
+  setTimeout(load, 1000);
+};
--- film/film.js
+++ film/film.js
@@ -1,15 +1,22 @@
 const parentDiv = document.querySelector(".parentDiv");
 const url = "https://65522b955c69a7790329a384.mockapi.io/movie/movie";
 
-async function createElemsInMenu(url) {
+async function getDataFromApi(url) {
   try {
     let response = await fetch(url);
+    let data = response.json();
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
 
+function createElemsInMenu(data) {
+  function drawAmenu(data) {
     data.forEach((obj) => {
       const elem = document.createElement("div");
       elem.setAttribute("class", "elemClass");
@@ -22,13 +29,61 @@ async function createElemsInMenu(url) {
       titleInElem.setAttribute("class", "spanStyleInElem");
       titleInElem.innerText =
         obj.title.length > 15 ? obj.title.slice(0, 15) + "..." : obj.title;
-
       elem.append(divPic, titleInElem);
-      return parentDiv.append(elem);
+      parentDiv.append(elem);
+
+      elem.addEventListener("click", async () => {
+        const uniqueData = await fetch(`${url}/${obj.id}`);
+        const uniqueElemList = await uniqueData.json();
+        const modal = showModal();
+        parentDiv.prepend(modal);
+
+        const divModalImg = document.querySelector(".divForModalImg");
+        const modalText = document.querySelector(".modalText");
+        const modalTextYear = document.querySelector(".modalTextYear");
+        const modalButton = document.querySelector(".modalCloseButton");
+        const divForVideo = document.querySelector(".divForVideo");
+        divModalImg.style.backgroundImage = `url(${uniqueElemList.big})`;
+        modalText.innerText = uniqueElemList.description;
+        modalTextYear.innerText = uniqueElemList.year;
+        modalButton.addEventListener("click", () => hideModal());
+        divForVideo.addEventListener("click", () =>
+          showModalVideo(uniqueElemList)
+        );
+      });
     });
-  } catch (error) {
-    console.error("Error fetching data:", error);
   }
+
+  drawAmenu(data);
+  const searchInput = document.querySelector(".searchInputClass");
+  searchInput.addEventListener("input", async (evt) => {
+    let data = await getDataFromApi(url);
+    data = data.filter((val) => {
+      return val.title.search(evt.target.value) !== -1;
+    });
+    parentDiv.innerHTML = "";
+
+    drawAmenu(data);
+  });
+}
+
+function navBar() {
+  const navBar = document.createElement("nav");
+  navBar.setAttribute("class", "nav");
+  const iconForBackManu = document.createElement("a");
+  iconForBackManu.href = "../index.html";
+  const imgBack = document.createElement("img");
+  imgBack.style.width = "57px";
+  imgBack.style.height = "64px";
+  imgBack.src =
+    "../images/kisspng-website-house-home-world-wide-web-computer-icons-house-clip-art-5ab036bbf19551.9166615015214977879895.png";
+  iconForBackManu.append(imgBack);
+  const searchInput = document.createElement("input");
+  searchInput.setAttribute("class", "searchInputClass");
+  searchInput.type = "search";
+  searchInput.placeholder = "search";
+  navBar.append(iconForBackManu, searchInput);
+  return navBar;
 }
 
 function showModal() {
@@ -67,20 +122,25 @@ function showModal() {
   );
   modalContent.append(divForModalImg, divForModalText);
   modal.append(modalContent);
+
   return modal;
 }
 
 function hideModal() {
   const modal = document.querySelector(".myModal");
-  modal.style.display = "none";
-  document.body.style.overflow = "auto";
+  modal.classList.add("closing");
+  setTimeout(() => {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
+    modal.remove();
+  }, 1000);
 }
 
 function showModalVideo(list) {
-  const modal = document.createElement("div");
+  const modal = document.createElement("iframe");
   modal.setAttribute("class", "myModalVideo");
   modal.style.display = "block";
-  modal.innerHTML = `<iframe heigth="70px" width="300px" src=${list.video}></iframe>`;
+  modal.src = list.video;
   const divForModalText = document.querySelector(".divForModalText");
   divForModalText.append(modal);
   const divForVideo = document.querySelector(".divForVideo");
@@ -88,30 +148,9 @@ function showModalVideo(list) {
 }
 
 async function init(url) {
-  await createElemsInMenu(url);
-  const elems = document.querySelectorAll(".elemClass");
-  elems.forEach((elem, i) => {
-    elem.addEventListener("click", async () => {
-      const uniqueData = await fetch(`${url}/${i + 1}`);
-      const uniqueElemList = await uniqueData.json();
-      const modal = showModal();
-      parentDiv.prepend(modal);
-      document.body.style.overflow = "hidden";
-
-      const divModalImg = document.querySelector(".divForModalImg");
-      const modalText = document.querySelector(".modalText");
-      const modalTextYear = document.querySelector(".modalTextYear");
-      const modalButton = document.querySelector(".modalCloseButton");
-      const divForVideo = document.querySelector(".divForVideo");
-      divModalImg.style.backgroundImage = `url(${uniqueElemList.big})`;
-      modalText.innerText = uniqueElemList.description;
-      modalTextYear.innerText = uniqueElemList.year;
-      modalButton.addEventListener("click", () => hideModal());
-      divForVideo.addEventListener("click", () =>
-        showModalVideo(uniqueElemList)
-      );
-    });
-  });
+  document.body.prepend(navBar());
+  const data = await getDataFromApi(url);
+  createElemsInMenu(data);
 }
 
 init(url);
