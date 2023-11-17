40fabd4f05355dd4a66dea99d2b5bc0de418980f
Grigor Harutyunyan <grigorharutyunian97@gmail.com>
2023-11-17 21:13:00 +0400

    add function loading and this style

--- books/script.js
+++ books/script.js
@@ -1,107 +1,113 @@
-const apiUrl = 'https://openlibrary.org/search.json?q=';
+const apiUrl = "https://openlibrary.org/search.json?q=";
 const resultsPerPage = 20;
 let currentPage = 1;
 
-document.getElementById('searchForm').addEventListener('submit', function (event) {
+document
+  .getElementById("searchForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
     searchBooks();
-});
+  });
 
-document.getElementById('pagination').addEventListener('click', function (event) {
-    if (event.target.tagName === 'BUTTON') {
-        const pageNumber = parseInt(event.target.textContent);
-        if (!isNaN(pageNumber)) {
-            getPage(pageNumber);
+document
+  .getElementById("pagination")
+  .addEventListener("click", function (event) {
+    if (event.target.tagName === "BUTTON") {
+      const pageNumber = parseInt(event.target.textContent);
+      if (!isNaN(pageNumber)) {
+        getPage(pageNumber);
 
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
-        }
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }
     }
-});
+  });
 
 function resetSearch() {
-    currentPage = 1;
-    const resultsContainer = document.getElementById('results');
-    const paginationContainer = document.getElementById('pagination');
+  currentPage = 1;
+  const resultsContainer = document.getElementById("results");
+  const paginationContainer = document.getElementById("pagination");
 
-    resultsContainer.innerHTML = '';
-    paginationContainer.innerHTML = '';
+  resultsContainer.innerHTML = "";
+  paginationContainer.innerHTML = "";
 }
 
 async function searchBooks() {
-    resetSearch();
+  resetSearch();
 
-    const bookInput = document.getElementById('bookInput').value;
-    const resultsContainer = document.getElementById('results');
-    const paginationContainer = document.getElementById('pagination');
+  const bookInput = document.getElementById("bookInput").value;
+  const resultsContainer = document.getElementById("results");
+  const paginationContainer = document.getElementById("pagination");
 
-    resultsContainer.innerHTML = '';
-    paginationContainer.innerHTML = '';
+  resultsContainer.innerHTML = "";
+  paginationContainer.innerHTML = "";
 
-    const apiUrlFirstPage = `${apiUrl}${bookInput}&limit=${resultsPerPage}`;
+  const apiUrlFirstPage = `${apiUrl}${bookInput}&limit=${resultsPerPage}`;
 
-    try {
-        const response = await fetch(apiUrlFirstPage);
-        const data = await response.json();
+  try {
+    const response = await fetch(apiUrlFirstPage);
+    const data = await response.json();
 
-        displayResults(data.docs);
+    displayResults(data.docs);
 
-        const totalPages = Math.ceil(data.numFound / resultsPerPage);
-        for (let i = 1; i <= totalPages; i++) {
-            const pageButton = document.createElement('button');
-            pageButton.textContent = i;
-            paginationContainer.appendChild(pageButton);
+    const totalPages = Math.ceil(data.numFound / resultsPerPage);
+    for (let i = 1; i <= totalPages; i++) {
+      const pageButton = document.createElement("button");
+      pageButton.textContent = i;
+      paginationContainer.appendChild(pageButton);
 
-            if (i === currentPage) {
-                pageButton.classList.add('active');
-            }
-        }
-
-        resultsContainer.innerHTML += `<div class="books__total-results" data-aos="flip-up" data-aos-delay="200" data-aos-duration="800" data-aos-easing="ease-in-out" data-aos-mirror="false">Total results: ${data.numFound}</div>`;
-    } catch (error) {
-        throw new Error('Error fetching data');
+      if (i === currentPage) {
+        pageButton.classList.add("active");
+      }
     }
+
+    resultsContainer.innerHTML += `<div class="books__total-results" data-aos="flip-up" data-aos-delay="200" data-aos-duration="800" data-aos-easing="ease-in-out" data-aos-mirror="false">Total results: ${data.numFound}</div>`;
+  } catch (error) {
+    throw new Error("Error fetching data");
+  }
 }
 
 async function getPage(pageNumber) {
-    currentPage = pageNumber;
+  currentPage = pageNumber;
 
-    const bookInput = document.getElementById('bookInput').value;
-    const apiUrlWithPage = `${apiUrl}${encodeURIComponent(bookInput)}&page=${pageNumber}&limit=${resultsPerPage}`;
+  const bookInput = document.getElementById("bookInput").value;
+  const apiUrlWithPage = `${apiUrl}${encodeURIComponent(
+    bookInput
+  )}&page=${pageNumber}&limit=${resultsPerPage}`;
 
-    try {
-        const response = await fetch(apiUrlWithPage);
-        const data = await response.json();
+  try {
+    const response = await fetch(apiUrlWithPage);
+    const data = await response.json();
 
-        displayResults(data.docs);
+    displayResults(data.docs);
 
-        updateActivePagination();
-    } catch (error) {
-        throw new Error('Error fetching data');
-    }
+    updateActivePagination();
+  } catch (error) {
+    throw new Error("Error fetching data");
+  }
 }
 
 function updateActivePagination() {
-    const paginationButtons = document.querySelectorAll('#pagination button');
-    paginationButtons.forEach((button, index) => {
-        if (index + 1 === currentPage) {
-            button.classList.add('active');
-        } else {
-            button.classList.remove('active');
-        }
-    });
+  const paginationButtons = document.querySelectorAll("#pagination button");
+  paginationButtons.forEach((button, index) => {
+    if (index + 1 === currentPage) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
 }
 
 function displayResults(books) {
-    const resultsContainer = document.getElementById('results');
+  const resultsContainer = document.getElementById("results");
 
-    resultsContainer.innerHTML = '';
+  resultsContainer.innerHTML = "";
 
-    books.forEach(book => {
-        resultsContainer.innerHTML += `
+  books.forEach((book) => {
+    resultsContainer.innerHTML += `
             <div class="books__result-item"
                  data-aos="fade-up"
                  data-aos-delay="200"
@@ -111,13 +117,27 @@ function displayResults(books) {
                  data-aos-once="true"
             >
                 <h2><strong>Title:</strong> ${book.title}</h2>
-                <h3><strong>Author Name:</strong> ${book.author_name ? book.author_name.join(', ') : 'N/A'}</h3>
-                <p><strong>First Publish Year:</strong> ${book.first_publish_year || 'N/A'}</p>
-                <p><strong>Subject:</strong> ${book.subject ? book.subject.slice(0, 5).join(', ') : 'N/A'}</p>
+                <h3><strong>Author Name:</strong> ${
+                  book.author_name ? book.author_name.join(", ") : "N/A"
+                }</h3>
+                <p><strong>First Publish Year:</strong> ${
+                  book.first_publish_year || "N/A"
+                }</p>
+                <p><strong>Subject:</strong> ${
+                  book.subject ? book.subject.slice(0, 5).join(", ") : "N/A"
+                }</p>
             </div>
         `;
-    });
+  });
 }
 
 // Initialize AOS
 AOS.init();
+
+function load() {
+  const load = document.getElementById("loading");
+  load.style.display = "none";
+}
+window.onload = function () {
+  setTimeout(load, 500);
+};
